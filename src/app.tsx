89558b2629f6252ca418c5ed0cc7491bac3d4ec1
Adão Lima <adaow1257@gmail.com>
2024-09-12 15:00:44 -0300

    Conexao com o backend

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,24 @@
+import { useEffect, useState } from 'react'
 import { Dialog } from './assets/components/ui/dialog'
 import { CreateGoal } from './assets/components/create-goal'
-//import { EmptyGoals } from './assets/components/empty-goals'
+
+import { EmptyGoals } from './assets/components/empty-goals'
 import { Summary } from './assets/components/summary'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
 
 export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60, // 60 segundos
+  })
+
   return (
     <Dialog>
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
       {/*<EmptyGoals /> */}
-      <Summary />
+      {/*<Summary />*/}
       <CreateGoal />
     </Dialog>
   )
